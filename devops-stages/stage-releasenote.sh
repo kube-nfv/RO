b95ba12801bee3584bfb24f12402e006d3893284
aticig <gulsum.atici@canonical.com>
2022-06-07 19:09:38 +0300

    Extracting stage releasenotes script from stage-test.sh
    Release notes checking in stage-test.sh causes the failure in  merge and revert jobs.
    So it is added as an separate script.
    
    Change-Id: Ie20878cb4822ba72e9d81f3e6ede8dcd1f65d99e
    Signed-off-by: aticig <gulsum.atici@canonical.com>

--- devops-stages/stage-releasenote.sh
+++ devops-stages/stage-releasenote.sh
@@ -12,8 +12,19 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 set -e
-echo "Launching tox"
-tox --parallel=auto
+echo "Checking the presence of release notes ..."
 
+nb_rn=$(git diff --diff-filter=A --name-only HEAD~1 |grep "releasenotes\/notes" |wc -l)
+if [ "${nb_rn}" -lt 1 ]; then
+    echo "The commit needs release notes"
+    echo "Run the following command to generate release notes: tox -e release_notes '<release_note_title>'"
+    echo "Please read README.md for more details"
+    exit 1
+elif [ "${nb_rn}" -gt 1 ]; then
+    echo "Only one release notes file should be added in a commit"
+    exit 1
+fi
 
+echo "OK. Release notes present in commit"
