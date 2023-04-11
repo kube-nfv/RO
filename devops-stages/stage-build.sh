7e3aaf484cf189dbcff888522c7766f85e975e4a
garciadeblas <gerardo.garciadeblas@telefonica.com>
2023-04-11 10:08:26 +0200

    Update stage-build to run tox sequentially
    Change-Id: I967f19a8c35700290e93c9d8bd863b63b7c2d239
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>
    (cherry picked from commit ea063c7a6ae6a5d7e11e8c22f9707d5c8f674ac7)

--- devops-stages/stage-build.sh
+++ devops-stages/stage-build.sh
@@ -37,9 +37,8 @@ dist_ro_vim_vmware
 dist_ro_vim_gcp"
 
 TOX_ENV_LIST="$(echo $PACKAGES | sed "s/ /,/g")"
-PROCESSES=$(expr `nproc --a` / 2)
 
-TOX_PARALLEL_NO_SPINNER=1 tox -e $TOX_ENV_LIST --parallel $PROCESSES
+TOX_PARALLEL_NO_SPINNER=1 tox -e $TOX_ENV_LIST
 
 # Copying packages
 # RO plugin
