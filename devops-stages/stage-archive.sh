1c89c08a0dd1c79b5adff3ac1cc123239762e06a
garciadeblas <gerardo.garciadeblas@telefonica.com>
2023-04-18 15:06:30 +0200

    Clean stage-archive.sh and use allowlist_extenals in tox.ini
    Change-Id: I18f0bc3e263063b5b1d2cf211f028f6bb0e4bceb
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- devops-stages/stage-archive.sh
+++ devops-stages/stage-archive.sh
@@ -17,6 +17,4 @@ rm -rf pool
 rm -rf dists
 mkdir -p pool/RO
 mv deb_dist/*.deb pool/RO/
-mkdir -p dists/unstable/RO/binary-amd64/
-apt-ftparchive packages pool/RO > dists/unstable/RO/binary-amd64/Packages
-gzip -9fk dists/unstable/RO/binary-amd64/Packages
+
