d846e8ffaeddd9ebe59ae126fcb61a814686443f
kayal2001 <kayalvizhi.v@tataelxsi.co.in>
2023-11-06 09:15:51 +0000

    Fix Bug 2303:NS instance name getting truncated in OpenStack
    Change-Id: Ifcd11c79289d8b792c944f8cb0de46e6d967dc04
    Signed-off-by: kayal2001 <kayalvizhi.v@tataelxsi.co.in>
    Signed-off-by: garciadeblas <gerardo.garciadeblas@telefonica.com>

--- NG-RO/osm_ng_ro/ns.py
+++ NG-RO/osm_ng_ro/ns.py
@@ -1611,9 +1611,9 @@ class Ns(object):
 
         extra_dict["params"] = {
             "name": "{}-{}-{}-{}".format(
-                indata["name"][:16],
-                vnfr["member-vnf-index-ref"][:16],
-                target_vdu["vdu-name"][:32],
+                indata["name"],
+                vnfr["member-vnf-index-ref"],
+                target_vdu["vdu-name"],
                 target_vdu.get("count-index") or 0,
             ),
             "description": target_vdu["vdu-name"],
@@ -1844,9 +1844,9 @@ class Ns(object):
 
         extra_dict["params"] = {
             "name": "{}-{}-{}-{}".format(
-                db_nsr["name"][:16],
-                vnfr["member-vnf-index-ref"][:16],
-                existing_vdu["vdu-name"][:32],
+                db_nsr["name"],
+                vnfr["member-vnf-index-ref"],
+                existing_vdu["vdu-name"],
                 existing_vdu.get("count-index") or 0,
             ),
             "description": existing_vdu["vdu-name"],
