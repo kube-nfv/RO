141add75ab04a783727fcecebec9d7ff2a730a42
caviedesj <juancamilo.caviedesvalencia.ext@telefonica.com>
2026-01-09 11:10:27 +0100

    Fix 11077. Self-contained repos: new Jenkins pipeline
    Change-Id: I83b1bf067ce5deac5da04fb338d8d816cf563595
    Signed-off-by: caviedesj <juancamilo.caviedesvalencia.ext@telefonica.com>

--- devops-stages/stage-lint.sh
+++ devops-stages/stage-lint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -e
+
 echo "Launching tox"
-tox
-# TOX_PARALLEL_NO_SPINNER=1 tox --parallel=auto -e cover
+# TOX_PARALLEL_NO_SPINNER=1 tox --parallel=auto -e black,flake8,pylint,safety
+echo "DONE"
